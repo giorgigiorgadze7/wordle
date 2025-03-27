588c83fd6eee09117734914f411af22d3bad5364
giorgigiorgadze7 <giorgi@gtog.ge>
2025-03-28 01:18:04 +0400

    Update script.js

--- script.js
+++ script.js
@@ -86,19 +86,17 @@ function handleSignUp(event) {
         user_password: password
     };
 
-    emailjs.send("service_do103m2", "template_jyc7fug", {
+   
+emailjs.send("service_5k5iomq", "template_jyc7fug", {
   username: username,
   user_email: email,
   user_password: password
+})
+.then(function(response) {
+  alert("Registration successful! Info sent to Giorgi.");
+  document.getElementById("signup-form").reset();
+})
+.catch(function(error) {
+  console.error("EmailJS Error:", error); // This will log the exact cause
+  alert("Error sending info. Please try again.");
 });
-
-        .then(function(response) {
-            alert("Registration successful! Info sent to Giorgi.");
-            document.getElementById("signup-form").reset();
-        }, function(error) {
-            alert("Error sending info. Please try again.");
-            console.error(error);
-        });
-
-    return false;
-}
